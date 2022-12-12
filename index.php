11a9e216b1ec2cb369492ec4194793392b0b526e
MicheleRossetti02 <michelerossetti07@gmail.com>
2022-12-12 17:32:42 +0100

    to fix

--- index.php
+++ index.php
@@ -91,15 +91,15 @@ $lettiera->set_weight(2, ' kg');
                 <?php  ?>
 
                 <div class="card col" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
+                    <img src="<?php ?>" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <h5 class="card-title"><?php ?></h5>
+                        <p class="card-text"><?php ?></p>
                     </div>
                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item">An item</li>
-                        <li class="list-group-item">A second item</li>
-                        <li class="list-group-item">A third item</li>
+                        <li class="list-group-item"><?php ?></li>
+                        <li class="list-group-item"><?php ?></li>
+                        <li class="list-group-item"><?php ?></li>
                     </ul>
 
                 </div>
